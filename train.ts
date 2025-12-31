74101a67be777cc6de1f417356c3d659d0c79b68
mirabdulloh <tursunovmirabdulla@gmail.com>
2025-12-31 08:18:18 -0800

    feat: task ZM

--- train.ts
+++ train.ts
@@ -1,8 +1,4 @@
 // Task ZJ
-
-import { count, log } from 'console';
-import { retry } from 'rxjs';
-
 // import { count } from "console";
 
 // function reduceNestedArray(arr:any[]){
@@ -22,15 +18,15 @@ import { retry } from 'rxjs';
 // console.log(reduceNestedArray([1, [1, 2, [4]]]));
 
 // Task ZK
- 
+
 // function delayHelloWorld(limit: number): Promise<number> {
 //     let count = 0;
-  
+
 //     return new Promise((resolve) => {
 //       const id = setInterval(() => {
 //         count++;
 //         console.log(`${count === limit ? " " : count }`);
-        
+
 //         if (count === limit) {
 //           clearInterval(id);
 //           resolve(count);
@@ -38,14 +34,20 @@ import { retry } from 'rxjs';
 //       }, 1000);
 //     });
 //   }
-  
+
 //   delayHelloWorld(5).then(console.log);
 
-// Task ZL 
+// Task ZL
 
-function stringToKebab(str:string):string{
-  return str.trim().toLowerCase().replace(/\s+/g, '-');
+// function stringToKebab(str: string): string {
+// 	return str.trim().toLowerCase().replace(/\s+/g, '-');
+// }
+// console.log(stringToKebab('I love Kebab'));
+
+// Task ZM
+
+function reverseInteger(num: number) {
+	return num.toString().split('').reverse().join();
 }
-console.log(stringToKebab("I love Kebab"));
 
-  
+console.log(reverseInteger(1245666778899));
