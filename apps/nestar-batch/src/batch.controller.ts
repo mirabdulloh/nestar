2c2211f39ceaec2e95c7af49e042a5408f53b2e4
mirabdulloh <tursunovmirabdulla@gmail.com>
2026-01-23 17:14:15 -0800

    feat: create batchRollBack, batchProperties, batchAgents job Schedulers logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,62 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
 export class BatchController {
-	constructor(private readonly nestarBatchService: BatchService) {}
+	private logger: Logger = new Logger('BatchController');
+	constructor(private readonly batchService: BatchService) {}
+
+	@Timeout(1000)
+	handleInterval() {
+		this.logger['context'] = 'SERVER READY';
+		this.logger.debug('BATCH SERVER READY!!!');
+	}
+
+	@Cron('00 * * * * *', { name: BATCH_ROLLBACK })
+	public async batchRollBack() {
+		try {
+			this.logger['context'] = BATCH_ROLLBACK;
+			this.logger.debug('EXECUTE ROLLBACK BATCH JOB');
+
+			await this.batchService.batchRollBack();
+		} catch (err) {
+			this.logger.error('ROLLBACK BATCH JOB FAILED', err);
+		}
+	}
+
+	@Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchProperties() {
+		try {
+			this.logger['context'] = BATCH_TOP_PROPERTIES;
+			this.logger.debug('EXECUTE TOP PROPERTIES BATCH JOB');
+			await this.batchService.batchProperties();
+		} catch (err) {
+			this.logger.error('batchProperties BATCH JOB FAILED', err);
+		}
+	}
+
+	@Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
+	public async batchAgents() {
+		try {
+			this.logger['context'] = BATCH_TOP_AGENTS;
+			this.logger.debug('EXECUTE TOP AGENTS BATCH JOB');
+			await this.batchService.batchAgents();
+		} catch (err) {
+			this.logger.error('Batch TopAgents', err);
+		}
+	}
+
+	/**
+  @INterval(1000)
+	handleInterval() {
+		this.logger.debug('INTERVAL TEST');
+	}
+  */
 
 	@Get()
 	getHello(): string {
-		return this.nestarBatchService.getHello();
+		return this.batchService.getHello();
 	}
 }
--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -2,6 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BatchService {
+	public async batchRollBack(): Promise<void> {
+		console.log('Executing batchRollBack...');
+	}
+
+	public async batchProperties(): Promise<void> {
+		console.log('Executing batchProperties...');
+	}
+
+	public async batchAgents(): Promise<void> {
+		console.log('Executing batchAgents...');
+	}
+
 	getHello(): string {
 		return 'Welcome to Nestar Batch Server!';
 	}
